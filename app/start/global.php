0146ac4f5c7906025fcf54aa3d43ea1470bdb307
SelrahcD <c.desneuf@gmail.com>
2013-05-01 16:45:22 +0200

    Add errors and bindings files

--- app/start/global.php
+++ app/start/global.php
@@ -53,6 +53,33 @@ App::error(function(Exception $exception, $code)
 	Log::error($exception);
 });
 
+
+/*
+|--------------------------------------------------------------------------
+| Application Error Handler
+|--------------------------------------------------------------------------
+|
+| Here you may handle any errors that occur in your application, including
+| logging them or displaying custom views for specific errors. You may
+| even register several error handlers to handle different types of
+| exceptions. If nothing is returned, the default error view is
+| shown, which includes a detailed stack trace during debug.
+|
+*/
+
+require __DIR__.'/../errors.php';
+
+/*
+|--------------------------------------------------------------------------
+| Application Bindings
+|--------------------------------------------------------------------------
+|
+| Here we can make all bindings necessary to the app
+|
+*/
+
+require __DIR__.'/../bindings.php';
+
 /*
 |--------------------------------------------------------------------------
 | Require The Filters File
