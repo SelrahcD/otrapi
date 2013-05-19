0465c4888ab2ccc1c1a28b9597a9f761e37f4b38
SelrahcD <c.desneuf@gmail.com>
2013-05-19 16:53:55 +0200

    Add fluent token repo tests

---
+++ app/tests/repositories/FluentTokenRepositoryTest.php
@@ -0,0 +1,45 @@
+<?php
+ 
+use Mockery as m;
+
+class FluentTokenRepositoryTest extends TestCase {
+
+	public function tearDown()
+	{
+		m::close();
+	}
+
+	public function testStoreStoresToken()
+	{
+		$mocks = $this->getMocks();
+		$repo = $this->getRepo($mocks);
+		$user = m::mock('User');
+		$token = m::mock('Token');
+		$token->shouldReceive('getId')->once()->andReturn('tokenId');
+		$token->shouldReceive('getUserId')->once()->andReturn(1);
+		$token->shouldReceive('getExpiration')->once()->andReturn(12345);
+
+		$attributes = array(
+			'id'         => 'tokenId',
+			'user_id'    => 1,
+			'expiration' => 12345,
+			);
+
+		$mocks['db']->shouldReceive('table')->once()->with('tokens')->andReturn($mocks['db']);
+		$mocks['db']->shouldReceive('insert')->once()->with($attributes);
+		$repo->store($token);
+	}
+
+	private function getMocks()
+	{
+		return array(
+			'db' => m::mock('Illuminate\Database\DatabaseManager'),
+			);
+	}
+
+	private function getRepo($mocks)
+	{
+		return new FluentTokenRepository($mocks['db']);
+	}
+
+}
