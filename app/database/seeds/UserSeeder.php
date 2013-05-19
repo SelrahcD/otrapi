33431ac43d19099bca5a81716840dc71a6277ca1
SelrahcD <c.desneuf@gmail.com>
2013-05-19 16:49:21 +0200

    Add user database seeder

---
+++ app/database/seeds/UserSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+class UserSeeder extends Seeder {
+
+	/**
+	 * Run the database seeds.
+	 *
+	 * @return void
+	 */
+	public function run()
+	{
+		$user = new User;
+		$user->email = 'c.desneuf@gmail.com';
+		$user->password = Hash::make('password');
+		$user->save();
+	}
+
+}
