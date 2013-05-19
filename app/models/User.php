005b4d1eec6aa49a3abaf9b38ce9fd0f5ee3313c
SelrahcD <c.desneuf@gmail.com>
2013-05-19 16:51:55 +0200

    Clean user model

--- app/models/User.php
+++ app/models/User.php
@@ -1,52 +1,18 @@
 <?php
 
-use Illuminate\Auth\UserInterface;
-use Illuminate\Auth\Reminders\RemindableInterface;
-
-class User extends Eloquent implements UserInterface, RemindableInterface {
+class User extends Eloquent {
 
 	/**
-	 * The database table used by the model.
+	 * The attributes that are mass assignable.
 	 *
-	 * @var string
+	 * @var array
 	 */
-	protected $table = 'users';
+	protected $fillable = array('id', 'email', 'password', 'created_at', 'updated_at');
 
 	/**
-	 * The attributes excluded from the model's JSON form.
+	 * The attributes that should be hidden for arrays.
 	 *
 	 * @var array
 	 */
 	protected $hidden = array('password');
-
-	/**
-	 * Get the unique identifier for the user.
-	 *
-	 * @return mixed
-	 */
-	public function getAuthIdentifier()
-	{
-		return $this->getKey();
-	}
-
-	/**
-	 * Get the password for the user.
-	 *
-	 * @return string
-	 */
-	public function getAuthPassword()
-	{
-		return $this->password;
-	}
-
-	/**
-	 * Get the e-mail address where password reminders are sent.
-	 *
-	 * @return string
-	 */
-	public function getReminderEmail()
-	{
-		return $this->email;
-	}
-
 }
