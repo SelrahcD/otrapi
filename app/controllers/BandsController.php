17db709130314e5291ace7273b735f4fbf8d1272
SelrahcD <c.desneuf@gmail.com>
2013-06-07 19:12:44 +0200

    Add Bands controller

---
+++ app/controllers/BandsController.php
@@ -0,0 +1,31 @@
+<?php
+
+class BandsController extends BaseController {
+
+	/**
+	 * Bands repository
+	 * 
+	 * @var BandRepositoryInterface
+	 */
+	protected $repo;
+
+	public function __construct(BandRepositoryInterface $repo)
+	{
+		$this->repo = $repo;
+	}
+	
+
+	public function create()
+	{
+		$band = $this->repo->make(Input::all());
+
+		if(!$band->validate())
+		{
+			throw new ValidationException($band->errors());
+		}
+
+		$this->repo->store($band);
+
+		return $band;
+	}
+}
---
+++ app/tests/controllers/BandsControllerTest.php
@@ -0,0 +1,52 @@
+<?php
+
+use Mockery as m;
+
+class BandsControllerTest extends TestCase {
+
+	public function tearDown()
+	{
+		m::close();
+	}
+
+	public function setUp()
+	{
+		parent::setUp();
+
+		$this->mocks = $this->getMocks();
+		$this->controller = $this->getController($this->mocks);
+	}
+
+	public function testCreateBandStoresBandIfValidAndReturnsIt()
+	{
+		Input::shouldReceive('all')->once()->andReturn($input = 'input');
+		$this->mocks['repo']->shouldReceive('make')->once()->with($input)->andReturn($band = m::mock('Band'));
+		$band->shouldReceive('validate')->once()->andReturn(true);
+		$this->mocks['repo']->shouldReceive('store')->once()->with($band);
+		$response = $this->controller->create();
+		$this->assertEquals($band, $response);
+	}
+
+	/**
+	 * @expectedException ValidationException
+	 */
+	public function testCreateThrowsValidationExceptionIfDataIsntValid()
+	{
+		Input::shouldReceive('all')->once()->andReturn($input = 'input');
+		$this->mocks['repo']->shouldReceive('make')->once()->with($input)->andReturn($band = m::mock('Band'));
+		$band->shouldReceive('validate')->once()->andReturn(false);
+		$band->shouldReceive('errors')->once()->andReturn($errors = array());
+		$this->controller->create();
+	}
+
+	protected function getMocks()
+	{
+		return array(
+			'repo' => Mockery::mock('BandRepositoryInterface'));
+	}
+
+	protected function getController(Array $mocks = array())
+	{
+		return new BandsController($mocks['repo']);
+	}
+}
