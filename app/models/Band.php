b660c6fc70a5e0cce7a06b1ce54bea6d7f511244
SelrahcD <c.desneuf@gmail.com>
2013-06-07 19:13:59 +0200

    Set fillable and visible properties

--- app/models/Band.php
+++ app/models/Band.php
@@ -1,6 +1,20 @@
 <?php
 
 class Band extends Ardent {
+
+	/**
+	 * The attributes that are mass assignable.
+	 *
+	 * @var array
+	 */
+	protected $fillable = array('name');
+
+	/**
+	 * The attributes that should be visible in arrays.
+	 *
+	 * @var arrays
+	 */
+	protected $visible = array('id', 'name', 'created_at', 'updated_at');
 	
 	
 }
