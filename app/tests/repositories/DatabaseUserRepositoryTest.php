c8797b6fca17c9ccf5796d0e0d1117787b2c037c
SelrahcD <c.desneuf@gmail.com>
2013-06-15 13:17:17 +0200

    Setup method

--- app/tests/repositories/DatabaseUserRepositoryTest.php
+++ app/tests/repositories/DatabaseUserRepositoryTest.php
@@ -4,6 +4,12 @@ use Mockery as m;
 
 class DatabaseUserRepositoryTest extends TestCase {
 
+	public function setUp()
+	{
+		$this->mocks = $this->getMocks();
+		$this->repo = $this->getRepo($this->mocks);
+	}
+
 	public function tearDown()
 	{
 		m::close();
@@ -11,80 +17,66 @@ class DatabaseUserRepositoryTest extends TestCase {
 
 	public function testMakeReturnsAUser()
 	{
-		$mocks = $this->getMocks();
-		$repo = $this->getRepo($mocks);
 		$attributes = array('email' => 'c.desneuf@gmail.com');
-		$mocks['model']->shouldReceive('newInstance')->once()->with($attributes)->andReturn($user = m::mock('User'));
-		$result = $repo->make($attributes);	
+		$this->mocks['model']->shouldReceive('newInstance')->once()->with($attributes)->andReturn($user = m::mock('User'));
+		$result = $this->repo->make($attributes);	
 		$this->assertInstanceOf('User', $result);
 	}
 
 	public function testStoreStoresUser()
 	{
-		$mocks = $this->getMocks();
-		$repo = $this->getRepo($mocks);
 		$user = m::mock('User');
 		$user->shouldReceive('save')->once();
-		$repo->store($user);
+		$this->repo->store($user);
 	}
 
 
 	public function testGetUserByCredentialsReturnsNullIfNotFound()
 	{
-		$mocks = $this->getMocks();
-		$repo = $this->getRepo($mocks);
-		$mocks['model']->shouldReceive('newQuery')->once()->andReturn($builder = m::mock('\Illuminate\Database\Eloquent\Builder'));
+		$this->mocks['model']->shouldReceive('newQuery')->once()->andReturn($builder = m::mock('\Illuminate\Database\Eloquent\Builder'));
 		$builder->shouldReceive('where')->once()->with('email', '=', 'c.desneuf@gmail.com')->andReturn($builder);
 		$builder->shouldReceive('first')->once()->andReturn(null);
-		$this->assertNull($repo->getUserByCredentials(array('email' => 'c.desneuf@gmail.com')));
+		$this->assertNull($this->repo->getUserByCredentials(array('email' => 'c.desneuf@gmail.com')));
 	}
 
 	public function testGetUserByCredentialsReturnsUserIfFound()
 	{
-		$mocks = $this->getMocks();
-		$repo = $this->getRepo($mocks);
-		$mocks['model']->shouldReceive('newQuery')->once()->andReturn($builder = m::mock('\Illuminate\Database\Eloquent\Builder'));
+		$this->mocks['model']->shouldReceive('newQuery')->once()->andReturn($builder = m::mock('\Illuminate\Database\Eloquent\Builder'));
 		$builder->shouldReceive('where')->once()->with('email', '=', 'c.desneuf@gmail.com')->andReturn($builder);
 		$builder->shouldReceive('first')->once()->andReturn(m::mock('User'));
-		$result = $repo->getUserByCredentials(array('email' => 'c.desneuf@gmail.com'));
+		$result = $this->repo->getUserByCredentials(array('email' => 'c.desneuf@gmail.com'));
 		$this->assertInstanceOf('User', $result);
 	}
 
 	public function testGetUserByTokenReturnsUserIfFoundAndTokenIsValid()
 	{
-		$mocks = $this->getMocks();
-		$repo = $this->getRepo($mocks);
-		$mocks['model']->shouldReceive('newQuery')->once()->andReturn($builder = m::mock('\Illuminate\Database\Eloquent\Builder'));
+		$this->mocks['model']->shouldReceive('newQuery')->once()->andReturn($builder = m::mock('\Illuminate\Database\Eloquent\Builder'));
 		$builder->shouldReceive('join')->once()->with('tokens', 'users.id', '=', 'tokens.user_id')->andReturn($builder);
 		$builder->shouldReceive('where')->once()->with('tokens.id', '=', 'token')->andReturn($builder);
 		$builder->shouldReceive('where')->once()->with('expiration', '>', 'DateTime')->andReturn($builder);
 		$builder->shouldReceive('first')->once()->andReturn(m::mock('User'));
-		$result = $repo->getUserByToken('token');
+		$result = $this->repo->getUserByToken('token');
 		$this->assertInstanceOf('User', $result);
 	}
 
 	public function testGetUserByTokenWithExpiredFalseReturnsUserIfFound()
 	{
-		$mocks = $this->getMocks();
-		$repo = $this->getRepo($mocks);
-		$mocks['model']->shouldReceive('newQuery')->once()->andReturn($builder = m::mock('\Illuminate\Database\Eloquent\Builder'));
+		$this->mocks['model']->shouldReceive('newQuery')->once()->andReturn($builder = m::mock('\Illuminate\Database\Eloquent\Builder'));
 		$builder->shouldReceive('join')->once()->with('tokens', 'users.id', '=', 'tokens.user_id')->andReturn($builder);
 		$builder->shouldReceive('where')->once()->with('tokens.id', '=', 'token')->andReturn($builder);
 		$builder->shouldReceive('first')->once()->andReturn(m::mock('User'));
-		$result = $repo->getUserByToken('token', false);
+		$result = $this->repo->getUserByToken('token', false);
 		$this->assertInstanceOf('User', $result);
 	}
 
 	public function testGetUserByTokenReturnsNullIfNotFoundOrTokenInvalid()
 	{
-		$mocks = $this->getMocks();
-		$repo = $this->getRepo($mocks);
-		$mocks['model']->shouldReceive('newQuery')->once()->andReturn($builder = m::mock('\Illuminate\Database\Eloquent\Builder'));
+		$this->mocks['model']->shouldReceive('newQuery')->once()->andReturn($builder = m::mock('\Illuminate\Database\Eloquent\Builder'));
 		$builder->shouldReceive('join')->once()->with('tokens', 'users.id', '=', 'tokens.user_id')->andReturn($builder);
 		$builder->shouldReceive('where')->once()->with('tokens.id', '=', 'token')->andReturn($builder);
 		$builder->shouldReceive('where')->once()->with('expiration', '>', 'DateTime')->andReturn($builder);
 		$builder->shouldReceive('first')->once()->andReturn(null);
-		$result = $repo->getUserByToken('token');
+		$result = $this->repo->getUserByToken('token');
 		$this->assertNull($result);
 	}
 
